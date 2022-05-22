0d0741c96b1f80ad02ad4f9c0176a58305b01d71
filipecalegario <fcac@cin.ufpe.br>
2022-05-22 01:40:25 -0300

    fix urlParams from index to main

--- js/index.js
+++ js/index.js
@@ -8,13 +8,17 @@ function login() {
         const usernameElement = document.getElementById("username");
         const passwordElement = document.getElementById("password");
 
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const global_selected_mode = urlParams.get('mode') || "projeto";
+
         const username = usernameElement.value;
         const password = passwordElement.value;
 
         auth(username, password).then((token) => {
             console.log(token);
             localStorage.setItem("strateegiaAccessToken", token);
-            location.href = "main.html";
+            location.href = `main.html?mode=${global_selected_mode}`;
         });
     });
 }
